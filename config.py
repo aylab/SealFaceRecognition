e3bf37af50b0727b8a9dea3dc12535bc22fee3b6
dgathogo <edgetech26@gmail.com>
2020-04-18 00:54:17 -0500

    reset learning rate

--- config.py
+++ config.py
@@ -86,7 +86,7 @@ learning_rate_strategy = 'step'
 
 # learning rate schedule
 learning_rate_schedule = {
-    0:      0.0001,
+    0:      0.001,
     #400:      0.01,
     #480:    0.001,
     #5000:   0.001,
