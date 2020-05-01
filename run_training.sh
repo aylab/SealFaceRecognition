9e297a05105198317898864f14fc533c6da8bc3a
dgathogo <edgetech26@gmail.com>
2020-05-01 13:56:47 -0500

    bug fix

--- run_training.sh
+++ run_training.sh
@@ -2,8 +2,9 @@
 
 a=$SECONDS
 python train.py -d ../photos/ -c config.py -n 5
-seal_seconds=$(( SECONDS - a ))
+seal_seconds=$(( $SECONDS - $a ))
+sleep 5
 b=$SECONDS
-python train.py -d ../photos/ -c config_primnet.py -s true -n 5 
-prim_seconds=$(( SECONDS - b ))
-echo 'sealnet running time: $seal_seconds \nprimnet running time: $prim_seconds\n'
+#python train.py -d ../photos/ -c config_primnet.py -s true -n 5 
+prim_seconds=$(( $SECONDS - $b ))
+echo "sealnet running time: $seal_seconds \n primnet running time: $prim_seconds\n"
