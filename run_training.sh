a0a03a2628baaf2c072adaa732c4927888b74a4a
dgathogo <edgetech26@gmail.com>
2020-05-01 11:49:43 -0500

    calculate time

--- run_training.sh
+++ run_training.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/bash
 
+a=$SECONDS
 python train.py -d ../photos/ -c config.py -n 5
-sleep 60 # wait for splits files to be written
+seal_seconds=$(( SECONDS - a ))
+b=$SECONDS
 python train.py -d ../photos/ -c config_primnet.py -s true -n 5 
+prim_seconds=$(( SECONDS - b ))
+echo 'sealnet running time: $seal_seconds \nprimnet running time: $prim_seconds\n'
