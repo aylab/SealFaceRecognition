e6bd77c35b37e418ac4232fc587f288e7fe0c1c6
dgathogo <edgetech26@gmail.com>
2020-04-30 23:07:33 -0500

    training

--- run_training.sh
+++ run_training.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
+#! /usr/bin/bash
 
 python train.py -d ../photos/ -c config.py -n 10 > out_sealnet.txt 2>error_sealnet.txt&
-sleep 10 # wait for splits files to be written
-python train.py -d ../photos/ -c config_primnet.py -s true -n 10>out_primnet.txt 2>error_primnet.txt&
+sleep 60 # wait for splits files to be written
+python train.py -d ../photos/ -c config_primnet.py -s true -n 10 >out_primnet.txt 2>error_primnet.txt&
