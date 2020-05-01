3da6c878700b9a5ee1ba56a5220f2b537889d9dd
dgathogo <edgetech26@gmail.com>
2020-04-30 22:59:13 -0500

    ignore output files

--- run_training.sh
+++ run_training.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-python train.py -d ../photos/ -c config.py -n 10 > seal_out.txt 2>seal_error.txt&
-sleep 15 # wait for splits files to be written
-python train.py -d ../photos/ -c config_primnet.py -s true -n 10 > prim_out.txt 2>prim_error.txt&
+python train.py -d ../photos/ -c config.py -n 10 > out_sealnet.txt 2>error_sealnet.txt&
+sleep 10 # wait for splits files to be written
+python train.py -d ../photos/ -c config_primnet.py -s true -n 10>out_primnet.txt 2>error_primnet.txt&
