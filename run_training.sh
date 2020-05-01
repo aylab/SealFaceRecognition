0b3cbfa33cd0a76f5d157a5ee0bd5667ba584dba
dgathogo <edgetech26@gmail.com>
2020-05-01 11:27:57 -0500

    no output redirect

--- run_training.sh
+++ run_training.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
-python train.py -d ../photos/ -c config.py -n 10 > out_sealnet.txt 2>error_sealnet.txt&
+python train.py -d ../photos/ -c config.py -n 5
 sleep 60 # wait for splits files to be written
-python train.py -d ../photos/ -c config_primnet.py -s true -n 10 >out_primnet.txt 2>error_primnet.txt&
+python train.py -d ../photos/ -c config_primnet.py -s true -n 5 
