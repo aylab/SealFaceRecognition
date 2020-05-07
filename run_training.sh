e72f3ce945da02849f50172b2a3b96785bc5b7a3
dgathogo <edgetech26@gmail.com>
2020-05-06 21:33:52 -0500

    time

--- run_training.sh
+++ run_training.sh
@@ -1,12 +1,10 @@
 #! /usr/bin/bash
 
 SECONDS=0
-sleep 5
-#python train.py -d ../photos/ -c config.py -n 5
-seal_seconds= $SECONDS
+python train.py -d ../photos/ -c config.py -n 10
+seal_seconds=$SECONDS
 
 SECONDS=0
-sleep 8
-#python train.py -d ../photos/ -c config_primnet.py -s true -n 5 
+python train.py -d ../photos/ -c config_primnet.py -s true -n 10 
 prim_seconds=$SECONDS
-echo -e "sealnet running time: $seal_seconds \nprimnet running time: $prim_seconds\n"
+echo -e "sealnet running time: $seal_seconds s \nprimnet running time: $prim_seconds s\n"
